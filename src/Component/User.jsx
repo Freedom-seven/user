e9b5f37844eec630fc339a69dd5f434d1b96ee26
FREEDOM <freewhynane62@gmail.com>
2021-07-06 13:32:58 +0000

    add componet

---
+++ src/Component/User.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const User = (user, index) => {
+    return (
+        <div className="user" key={index}>
+            <h1>Name: {user.name}</h1>
+            <p>Email: {user.email}</p>
+            <p>Gen: {user.gen}</p>
+            <hr />
+        </div>
+    )
+}
+
+export default User;
