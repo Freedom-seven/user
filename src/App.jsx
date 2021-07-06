6d8f071ba6419f3996b1a36b69e479bb781705f3
FREEDOM <freewhynane62@gmail.com>
2021-07-06 13:22:42 +0000

    Add map

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import React, { Component } from 'react';
 class App extends Component {
     constructor(props) {
         super(props)
-        this.state =[
+        this.state ={
+        users: [
             {
                 name: "Freedom",
                 email: "example@email.com",
                 gen: 18
+                
             },
             {
                 name: "Bill",
@@ -21,11 +23,25 @@ class App extends Component {
             }
         ]
     }
+    }
     render() {
+        
+          const users = this.state.users.map((user, index) => {
+              return (
+                <div className="user" key={index}>
+                    <h1>Name: {user.name}</h1>
+                    <p>Email: {user.email}</p>
+                    <p>Gen: {user.gen}</p>
+                    <hr />
+                </div>
+              )
+          });
+        
         return (
             <div>
-                
+                {users}
             </div>
+            
         );
     }
 }
