087925c3480108424ab5718013da2fb883c93f26
FREEDOM <freewhynane62@gmail.com>
2021-07-06 13:50:45 +0000

    add return

--- src/Component/User.jsx
+++ src/Component/User.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 
-const User = (user, index) => {
+const User = ({user, index}) => {
     return (
         <div className="user" key={index}>
-            <h1>Name: {user.name}</h1>
+            <h3>Name: {user.name}</h3>
             <p>Email: {user.email}</p>
             <p>Gen: {user.gen}</p>
             <hr />
         </div>
-    )
+    );
 }
 
 export default User;
