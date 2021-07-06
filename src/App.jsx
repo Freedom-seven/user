4b089ec87fc97f3ec6e0f997476c7bcf77e2356a
FREEDOM <freewhynane62@gmail.com>
2021-07-06 12:21:41 +0000

    add state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+    constructor(props) {
+        super(props)
+        this.state =[
+            {
+                name: "Freedom",
+                email: "example@email.com",
+                gen: 18
+            },
+            {
+                name: "Bill",
+                email: "bill@email.com",
+                gen: 5
+            },
+            {
+                name: "Emma",
+                email: "emma@email.com",
+                gen: 19
+            }
+        ]
+    }
     render() {
         return (
             <div>
