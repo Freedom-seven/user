3b9d095dc35fcfb810449bb65443048801a351cb
FREEDOM <freewhynane62@gmail.com>
2021-07-06 14:00:31 +0000

    add contrustor

---
+++ src/Component/AddForm.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+class AddForm extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: "",
+            email: "",
+            gen: ""
+        }
+    }
+    render() {
+        return (
+            <div>
+                
+            </div>
+        );
+    }
+}
+
+export default AddForm;
