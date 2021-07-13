61bef5fab69de744afe4bd954ae27909fde22c69
FREEDOM <freewhynane62@gmail.com>
2021-07-13 19:11:23 +0000

    add props for delete

--- src/Component/UserList.jsx
+++ src/Component/UserList.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import User from './User';
+import React from "react";
+import User from "./User";
 
-const UserList = ({users, deleteUser}) => {
-    const userList = users.map((user) => {
-        return (
-         <User user={user} key={user.id} deleteUser={deleteUser} />
-        )
-    });
-
+const UserList = ({ users, deleteUser, editUser }) => {
+  const userList = users.map((user) => {
     return (
-        <div>
-            {userList}
-        </div>
+      <User
+        user={user}
+        key={user.id}
+        deleteUser={deleteUser}
+        editUser={editUser}
+      />
     );
-}
+  });
+
+  return <div className="user-list">{userList}</div>;
+};
 
 export default UserList;
