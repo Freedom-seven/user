97b97310d51e0dbda2c3bc0e777dbdbbc8ca97cb
FREEDOM <freewhynane62@gmail.com>
2021-07-13 19:10:52 +0000

    add button

--- src/Component/User.jsx
+++ src/Component/User.jsx
@@ -1,26 +1,53 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card'
-import ListGroup from 'react-bootstrap/ListGroup';
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import ListGroup from "react-bootstrap/ListGroup";
+import Modal from "react-bootstrap/Modal";
+import EditAddUserForm from "./EditAddUserForm";
 
-const User = ({user, deleteUser}) => {
-    const handleDelete = (e) => {
-        deleteUser(user.id)
-    }
-    return (
-        <div className="user">
+const User = ({ user, deleteUser, editUser }) => {
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  const handleDelete = (e) => {
+    deleteUser(user.id);
+  };
+
+  return (
+    <>
+      <div className="user">
         <Card>
-            <ListGroup variant="flush">
-                <ListGroup.Item>Name: {user.name}</ListGroup.Item>
-                <ListGroup.Item>Email: {user.email}</ListGroup.Item>
-                <ListGroup.Item>Gen: {user.gen}</ListGroup.Item>
-                <div className="card-btn">
-                    <button className="btn btn-success edit" >Edit</button>
-                    <button className="btn btn-danger delete" onClick={handleDelete} >Delete</button>
-                </div>
-            </ListGroup>
+          <ListGroup variant="flush">
+            <ListGroup.Item>Name: {user.name}</ListGroup.Item>
+            <ListGroup.Item>Email: {user.email}</ListGroup.Item>
+            <ListGroup.Item>Gen: {user.gen}</ListGroup.Item>
+            <div className="card-btn">
+              <button className="btn btn-success edit" onClick={handleShow}>
+                Edit
+              </button>
+              <button className="btn btn-danger delete" onClick={handleDelete}>
+                Delete
+              </button>
+            </div>
+          </ListGroup>
         </Card>
-        </div>
-    );
-}
+
+        <Modal show={show} onHide={handleClose} animation={false}>
+          <Modal.Header closeButton>
+            <Modal.Title>Edit User</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <EditAddUserForm
+              user={user}
+              editUser={editUser}
+              closeModal={handleClose}
+            />
+          </Modal.Body>
+        </Modal>
+      </div>
+    </>
+  );
+};
 
 export default User;
