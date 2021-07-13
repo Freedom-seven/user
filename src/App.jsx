1873f7f53473508103c64b43422da356f90452ff
FREEDOM <freewhynane62@gmail.com>
2021-07-13 15:09:54 +0000

    add handleEditUser

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,49 @@
-import React, { Component } from 'react';
-import AddForm from "./Component/AddForm"
-import UserList from './Component/UserList';
+import React, { Component } from "react";
+import AddForm from "./Component/AddForm";
+import UserList from "./Component/UserList";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state ={
-        users: []
-    }
-    }
-
-    handleAddUser = (newUser) => {
-        newUser.id = Math.random().toString();
-        this.setState({
-            users: [...this.state.users, newUser]
-        })
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
     };
+  }
 
-    handleDeleteUser = (userId) => {
-        
-    }
+  handleAddUser = (newUser) => {
+    newUser.id = Math.random().toString();
+    this.setState({
+      users: [...this.state.users, newUser],
+    });
+  };
 
-    render() {  
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-4">
-                    <AddForm addUser={this.handleAddUser} />
-                    </div>
-                    <div className="col-8">
-                    <UserList users={this.state.users} deleteUser={this.handleDeleteUser} />
-                    </div>
-                </div>
-            </div>
-            
-        );
-    }
+  handleDeleteUser = (userId) => {
+    const savedUsers = this.state.users.filter((user) => {
+      return user.id !== userId;
+    });
+    this.setState({ users: savedUsers });
+  };
+
+  handleEditUser = () => {};
+
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-4">
+            <AddForm addUser={this.handleAddUser} />
+          </div>
+          <div className="col-8">
+            <UserList
+              users={this.state.users}
+              deleteUser={this.handleDeleteUser}
+              editUser={this.handleEditUser}
+            />
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
