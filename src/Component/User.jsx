3432bb93f2b0ee941aaaf3d7c0209ffab8b57cd9
FREEDOM <freewhynane62@gmail.com>
2021-07-13 14:58:02 +0000

    add card

--- src/Component/User.jsx
+++ src/Component/User.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
+import Card from 'react-bootstrap/Card'
+import ListGroup from 'react-bootstrap/ListGroup';
 
-const User = ({user, index}) => {
+const User = ({user, deleteUser}) => {
+    const handleDelete = (e) => {
+        deleteUser(user.id)
+    }
     return (
-        <div className="user" key={index}>
-            <h3>Name: {user.name}</h3>
-            <p>Email: {user.email}</p>
-            <p>Gen: {user.gen}</p>
-            <hr />
+        <div className="user">
+        <Card>
+            <ListGroup variant="flush">
+                <ListGroup.Item>Name: {user.name}</ListGroup.Item>
+                <ListGroup.Item>Email: {user.email}</ListGroup.Item>
+                <ListGroup.Item>Gen: {user.gen}</ListGroup.Item>
+                <div className="card-btn">
+                    <button className="btn btn-success edit" >Edit</button>
+                    <button className="btn btn-danger delete" onClick={handleDelete} >Delete</button>
+                </div>
+            </ListGroup>
+        </Card>
         </div>
     );
 }
