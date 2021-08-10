0995ad5606d88d01855cbe763d92dab3bb583893
FREEDOM <freewhynane62@gmail.com>
2021-08-10 11:21:03 +0000

    import Provider and store

--- src/index.js
+++ src/index.js
@@ -4,12 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Provider } from 'react-redux';
+import store from "./store/store";
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
+  <Provider store = {store}>
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
 
