20b5333e406b9a11c9e734a96fa6e6d91a3e4511
FREEDOM <freewhynane62@gmail.com>
2021-08-10 11:21:16 +0000

    add reducer

---
+++ src/reducers/usersReducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+    users: []
+}
+
+const usersRuducers = (state = initialState, action) => {
+    switch (action.type) {
+        case "ADD_USER":
+            return state
+            
+    
+        default:
+            return state;
+    }
+}
+
+export default usersRuducers
