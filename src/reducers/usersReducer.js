0941744ae091ec30b6f5dbc5d4a15c0f338c4185
FREEDOM <freewhynane62@gmail.com>
2021-08-24 12:26:48 +0000

    add edit and delete user to reducer

--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -7,6 +7,19 @@ const usersRuducers = (state = initialState, action) => {
     case "ADD_USER":
       return { ...state.users, users: [...state.users, action.payload] };
 
+    case "EDIT_USER":
+      const newUsers =  state.users.map((user) =>
+        user.id === action.payload.id ? action.payload : user
+      )
+     
+     return {...state, users: newUsers};
+     
+    case "DELETE_USER":
+      const savedUsers = state.users.filter((user) => {
+        return user.id !== action.payload;
+      });
+      return {...state, users: savedUsers};
+
     default:
       return state;
   }
