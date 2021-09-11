59091b482f6b127ab130f8739b7c4e266312b2a5
FREEDOM <freewhynane62@gmail.com>
2021-09-11 12:08:47 +0000

    add firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAe3V2pQi5D-gQghDOSIMSvuxa6YZc88I0",
+    authDomain: "user-app-a8400.firebaseapp.com",
+    projectId: "user-app-a8400",
+    storageBucket: "user-app-a8400.appspot.com",
+    messagingSenderId: "642889958251",
+    appId: "1:642889958251:web:aa2f50f49b78ab0937422e"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+export default firebase;
