3fe00bb1cf7b4dd95e6346d2e16067211c39b633
FREEDOM <freewhynane62@gmail.com>
2021-09-14 13:40:28 +0000

    connect to firebase database

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,21 +1,33 @@
+import {getFirestore} from "redux-firestore";
+
 export const addUser = (user) => {
-    user.id = Math.random().toString();
-    return {
+  user.id = Math.random().toString();
+
+  return (distatch, state, { getFirestore }) => {
+    getFirestore()
+      .collection("users")
+      .add(user)
+      .then((docs) => {
+        console.log(docs);
+      });
+  };
+
+  /*return {
         type: "ADD_USER",
         payload: user
-    }
-}
+    }*/
+};
 
 export const editUser = (updatedUser) => {
-    return {
-        type: "EDIT_USER",
-        payload: updatedUser
-    }
-}
+  return {
+    type: "EDIT_USER",
+    payload: updatedUser,
+  };
+};
 
 export const deleteUser = (user_id) => {
-    return {
-        type: "DELETE_USER",
-        payload: user_id
-    }
-}
+  return {
+    type: "DELETE_USER",
+    payload: user_id,
+  };
+};
