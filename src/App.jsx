cc40160f98e61462ef93f91c422587dc2c0dab15
FREEDOM <freewhynane62@gmail.com>
2021-10-07 12:35:09 +0000

    render Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,15 @@
 import React, { Component } from "react";
-import AddForm from "./Component/AddForm";
-import UserList from "./Component/UserList";
-import './App.css';
+import Router from "./Router";
+
 
 class App extends Component {
 
-  handleAddUser = (newUser) => {
-    newUser.id = Math.random().toString();
-    this.setState({
-      users: [...this.state.users, newUser],
-    });
-  };
-
-  handleDeleteUser = (userId) => {
-    const savedUsers = this.state.users.filter((user) => {
-      return user.id !== userId;
-    });
-    this.setState({ users: savedUsers });
-  };
-
-  handleEditUser = (updatedUser) => {
-    this.setState({
-      users: this.state.users.map((user) =>
-        user.id === updatedUser.id ? updatedUser : user
-      ),
-    });
-  };
-
   render() {
     return (
-      <div className="container user">
-        <div className="row">
-          <div className="col-4">
-            <AddForm addUser={this.handleAddUser} />
-          </div>
-          <div className="col-8">
-            <UserList className="user-lists"/>
-          </div>
-        </div>
-      </div>
-    );
+      <>
+      <Router />
+    </>
+    ) 
   }
 }
 
