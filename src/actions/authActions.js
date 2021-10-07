a82243e5f355ce38129669b3c8339a82857e907a
FREEDOM <freewhynane62@gmail.com>
2021-10-07 23:27:46 +0000

    re-add google

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -23,24 +23,11 @@ export const loginWithGoogle = () => {
       .auth()
       .signInWithPopup(provider)
       .then((result) => {
-        /** @type {firebase.auth.OAuthCredential} */
-        var credential = result.credential;
-
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = credential.accessToken;
-        // The signed-in user info.
         var user = result.user;
-        // ...
+        console.log(user);
       })
       .catch((error) => {
-        // Handle Errors here.
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
+        console.log(error);
       });
   };
 };
@@ -57,7 +44,7 @@ export const loginWIthEmail = (email, password, replace) => {
       .catch((err) => {
         console.log(err);
       });
-  };
+  }; 
 };
 
 export const logout = (replace) => {
